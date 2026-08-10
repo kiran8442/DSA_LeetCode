19ab913c43708ec66e46509c0e650d377edf0b21
Kiran Pawar <73823307+kiran8442@users.noreply.github.com>
2026-08-10 17:55:51 +0530

    Time: 0 ms (100.00%) | Memory: 30.4 MB (18.22%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -26,21 +26,5 @@ public:
             nums[index] = nums2[index];
             index++;
         }
-
-
-
-        /*k = k % nums.size();
-        int index = 0;
-        int temp;
-    
-        while(index < k){
-            temp = nums[nums.size() - 1];
-            for(int i = nums.size() - 2; i >= 0; i--)
-            {
-                nums[i + 1] = nums[i];
-            }
-            nums[0] = temp;
-            index++;
-        }*/
     }
 };
